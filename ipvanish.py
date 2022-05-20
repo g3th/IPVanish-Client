433316dcf4b7472a4adb550307fb8a7583cbb8a7
g3th <theinkandpen@yahoo.co.uk>
2022-05-20 03:12:27 +0100

    modified:   ipvanish.py 	modules/ 	test/

--- ipvanish.py
+++ ipvanish.py
@@ -2,35 +2,21 @@ import os
 import requests
 import subprocess
 import shlex
+import modules.dlconfig as config
 
-print('\x1bc')
+print("\x1bc")
 
-ctr=0 ; progress='#';files=[]; path='/home'
+setup = config.dlconfigs()
 
-page='https://www.ipvanish.com/software/configs/configs.zip'
+setup.check_dirs()
 
-print("Checking Directories")
+print('Checking Directories...\n')
 
-directory_=subprocess.check_output(shlex.split ('find /home -type d -name "IPVanish-Client"'), shell=False)
+setup.create_dirs()
 
-directory = str(directory_).strip("b'").strip("\\n")
-configs=directory+"/configs"
+if setup.create_dirs()[2] == 1:
 
-if 'IPVanish-Client' in directory:
-	
-	print('Client found in: ' + directory)
+	print('Client found in: '+ setup.create_dirs()[1])
 
+setup.subprocess()
 
-if 'configs' not in os.listdir(directory):
-	
-	print('Created "configs" directory \n')
-
-	os.mkdir (directory + "/configs")
-
-print("Downloading/Unzipping Config files")
-
-subprocess.run(shlex.split('wget "'+page+'" -P '+directory+'/configs/'), shell=False, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-
-subprocess.run(shlex.split('unzip '+configs+'/configs.zip'+' -d '+configs), shell=False, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-
-subprocess.run(shlex.split('rm '+configs+'/configs.zip'))
