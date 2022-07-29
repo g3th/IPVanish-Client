fa1e5dc9d2fbf7dfaffc57d83bdd28126cf36770
g3th <theinkandpen@yahoo.co.uk>
2022-07-29 22:27:46 +0100

    modified:   ../modules/dlconfig.py

--- modules/dlconfig.py
+++ modules/dlconfig.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import concurrent.futures
+import shutil
 
 from threading import Lock
 from pathlib import Path
@@ -14,30 +15,41 @@ class configs_tab:
 		self.directory = str(Path(__file__).parents[1])+'/configs/'
 		self.download_link = 'https://configs.ipvanish.com/configs/'
 		self.file_number = 0
-		self.lock = Lock()
+		self.lock = Lock()			# Python Mutual Exclusive Lock (GIL)
 		self.tab_four = ttk.Frame(root)
 		notebook.add(self.tab_four, text = 'Configs')
+		self.executor = concurrent.futures.ThreadPoolExecutor(20)
+		self.check_if_configs_exist()
+		
+	def draw_the_progressbar(self):
+	
 		self.progress_bar = ttk.Progressbar(self.tab_four, orient = 'horizontal', mode = 'determinate', length = 100)
 		self.progress_bar_title = Label(self.tab_four, text = 'Download Progress', font=('Arial',13))
 		self.progress_bar_title.place(x=56,y=22)
 		self.progress_bar.place(height=25,width=200,x=40,y=50)
-		self.executor = concurrent.futures.ThreadPoolExecutor(20)
-		self.check_if_configs_exist()
 		
 	def check_if_configs_exist(self):
+		self.draw_the_progressbar()
 		while True:
 			try:
 				if len(os.listdir(self.directory)) > 1:
-					label_test = Label(self.tab_four, font=('Arial',13),text='Config Files Already Exist')
-					label_test.place(x=33,y=78)
+					self.progress_bar_title.config(font=('Arial',13),text='Config Files Found!')
+					self.delete_and_download_again_button = Button(self.tab_four, text = 'Delete and Re-Download Configs', command = self.delete_and_download_again)
+					self.delete_and_download_again_button.place(x=20,y=90)				
+					self.progress_bar.destroy()
 					break
 				else:
-					self.configs_button = Button(self.tab_four, text='Download Open-VPN Config Files',command=self.threaded_downloads)
-					self.configs_button.place(x=20,y=90)
+					self.configs_download_button = Button(self.tab_four, text='Download Open-VPN Config Files',command=self.threaded_downloads)
+					self.configs_download_button.place(x=20,y=90)
 					break
 			except OSError:
 				os.mkdir (self.directory)
 				pass
+	def delete_and_download_again(self):
+		self.draw_the_progressbar()
+		shutil.rmtree(self.directory)
+		self.delete_and_download_again_button.destroy()
+		self.threaded_downloads()
 						
 	def fetch_and_parse_download_links(self):	
 		self.links_list=[]
@@ -51,18 +63,19 @@ class configs_tab:
 		download_request = requests.get(self.links_list[download_link]).content
 		filename = str(self.links_list[download_link].split('/')[4])
 		with open(self.directory + filename , 'wb') as zipped_config:
-			zipped_config.write(download_request)			
+			zipped_config.write(download_request)		
 		zipped_config.close()
 		
 	def fill_up_the_progressbar(self, future):
-		self.lock.acquire()
+		self.lock.acquire()					# the Mutex Prevents a Race condition
 		self.file_number +=1
 		self.progress_bar['value'] += 1
 		self.progress_bar_title.config(text = 'File {}/{}'.format(str(self.file_number),str(len(self.links_list))))
-		self.lock.release()
+		self.lock.release()					# Release the Kraken
 				
 	def threaded_downloads(self):
 		self.fetch_and_parse_download_links()
+		self.configs_download_button.destroy()
 		self.progress_bar.config(maximum = len(self.links_list))
 		futures = [self.executor.submit(self.download_ovpn_files, link) for link in range(len(self.links_list))]
 		for future in futures:
